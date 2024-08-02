4285bebf427d2c273c87fd3b9743c29c08b85fbe
Kovalski <andre.kovalski@tecpuc.com.br>
2024-08-02 13:56:12 -0300

    testes

--- scripts/check-version.js
+++ scripts/check-version.js
@@ -7,6 +7,9 @@ function run() {
     const tauriVersion = tauriConfig.package.version;     
     const currentTagVersion = process.argv[2].split('v')[1];
     const exec = require('child_process').exec;
+    if(!currentTagVersion){
+        throw (`Something went very very VERY wrong, and we got no versions from the latest release. Got [${process.argv[2]}], version [${currentTagVersion}]`)
+    }
     exec(`npm version`, (err, stdout) => {
         const packageVersion = stdout.split('\n')[1].split('\'')[3];
         if(packageVersion !== tauriVersion){
