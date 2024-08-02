dfc6c2e6373fd3f78c8a7bc7be3356ef5f8a2ea9
Kovalski <andre.kovalski@tecpuc.com.br>
2024-08-02 11:49:08 -0300

    script e workflow para checar versão do projeto

---
+++ scripts/check-version.js
@@ -0,0 +1,19 @@
+const tauriConfigFile = '../src-tauri/tauri.conf.json';
+
+run();
+
+function run() {
+    let tauriConfig = require(tauriConfigFile);
+    const tauriVersion = tauriConfig.package.version;     
+    const currentTagVersion = process.argv[2].split('v')[1];
+    const exec = require('child_process').exec;
+    exec(`npm version`, (err, stdout) => {
+        const packageVersion = stdout.split('\n')[1].split('\'')[3];
+        if(packageVersion !== tauriVersion){
+            throw ('tauri and package version mismatch!\nPlease run \'npm run ver p\' to fix this problem')
+        }
+        if(tauriVersion === currentTagVersion){
+            throw ('The new release must have a different version number than the last one!\nPlease run \'npm run ver p\' to fix this problem')
+        }
+    });
+}
