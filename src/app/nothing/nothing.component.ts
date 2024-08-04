425827ddd0879a94968fbed29b051e8bb65381fd
Kovalski <andre.kovalski@tecpuc.com.br>
2024-08-04 16:35:26 -0300

    iniciando trabalho com phaser

---
+++ src/app/nothing/nothing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nothing',
+  standalone: true,
+  imports: [],
+  templateUrl: './nothing.component.html',
+  styleUrl: './nothing.component.scss'
+})
+export class NothingComponent {
+
+}
