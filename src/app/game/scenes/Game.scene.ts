a705650d63ac783808274cc0d476ffea0c200961
Kovalski <andre.kovalski@tecpuc.com.br>
2024-08-05 00:41:32 -0300

    testes interação com jogador

--- src/app/game/scenes/Game.scene.ts
+++ src/app/game/scenes/Game.scene.ts
@@ -1,8 +1,13 @@
 export class GameScene extends Phaser.Scene {
 
+    public pressArea: Phaser.GameObjects.Rectangle; 
     public limit: Phaser.GameObjects.Rectangle;
     public notes: Phaser.Physics.Arcade.Group;
     public readonly noteSprite: string = "note";
+    public score: number;
+    public inputs: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+    public isPressed: boolean | undefined = false;
+    public scoreText: Phaser.GameObjects.Text;
 
     constructor() {
         super({key: 'game'});
@@ -11,12 +16,18 @@ export class GameScene extends Phaser.Scene {
     preload() {
         this.add.image(0, 0, 'background').setOrigin(0, 0);
         this.notes = this.physics.add.group();
-        for(let i = 0; i < 17; i++){
+        for(let i = 0; i < 5; i++){
             this.createNote(i);
         }
 
         this.limit = this.add.rectangle(285, 127, 2, 457).setOrigin(0,0);
+        this.pressArea = this.add.rectangle(287, 127, 60, 457).setOrigin(0,0);
         this.physics.add.existing(this.limit, true);
+        this.physics.add.existing(this.pressArea, true);
+        this.score = 0;
+
+        this.inputs = this.input.keyboard?.createCursorKeys();
+        this.scoreText = this.add.text(50, 50, '', { color: 'white' }).setOrigin(0, 0);
     }
 
     create() {
@@ -25,6 +36,20 @@ export class GameScene extends Phaser.Scene {
 
     override update() {
         this.physics.overlap(this.notes, this.limit, this.removeNote, undefined, this);
+        this.physics.overlap(this.notes, this.pressArea, this.pressedNote, undefined, this);
+        if(this.isPressed && this.inputs?.space.isUp) {
+            this.isPressed = false;
+        }
+        this.scoreText.setText(`Score: ${this.score}`);
+    }
+
+    public pressedNote(area: any, note: any){
+        if(!this.isPressed && this.inputs?.space.isDown) {
+            this.isPressed = true;
+            note.destroy();
+            this.createNote();
+            this.score += 10;
+        }
     }
 
     public createNote(y: number = -1): void {
@@ -41,6 +66,7 @@ export class GameScene extends Phaser.Scene {
     public removeNote(limit: any, note: any): void {
         note.destroy();
         this.createNote();
+        this.score -= 1;
     }
 
 }
